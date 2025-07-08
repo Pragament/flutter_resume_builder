cbc1fe06d0571ee83556bf4cdf7732b85397691d
taker999 <116102013+taker999@users.noreply.github.com>
2025-07-08 18:15:51 +0530

    added search box to search repo list (#40)

--- lib/views/repo_list_screen.dart
+++ lib/views/repo_list_screen.dart
@@ -2,11 +2,14 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:resume_builder_app/auth_provider.dart';
 import 'package:resume_builder_app/data/git_operations.dart';
 import 'package:resume_builder_app/models/git_repo_model.dart';
 import 'package:resume_builder_app/providers/repo_provider.dart';
 import 'package:resume_builder_app/views/repo_contents_screen.dart';
+
+import 'widgets/bg_gradient_color.dart';
 
 class RepoListScreen extends ConsumerStatefulWidget {
   const RepoListScreen({super.key});
@@ -16,38 +19,95 @@ class RepoListScreen extends ConsumerStatefulWidget {
 }
 
 class _RepoListScreenState extends ConsumerState<RepoListScreen> {
+  AuthStatus authStatus = AuthStatus.loading;
 
-  AuthStatus authStatus=AuthStatus.loading;
+  final TextEditingController _searchController = TextEditingController();
+  final List<dynamic> _filteredRepos = [];
 
   @override
   void initState() {
     initialize();
     super.initState();
   }
-  Future<void> initialize()async{
-    final repo=ref.read(repoProvider);
+
+  Future<void> initialize() async {
+    final repo = ref.read(repoProvider);
     await repo.getToken();
     await repo.fetchRepositories(true);
+
+    _filteredRepos.addAll(repo.repositories);
+  }
+
+  void _onSearchChanged(String value) {
+    final repos = ref.read(repoProvider);
+    final query = value.toLowerCase();
+
+    setState(() {
+      _filteredRepos.clear();
+      _filteredRepos.addAll(
+        repos.repositories.where(
+          (repo) => repo['name'].toLowerCase().contains(query),
+        ),
+      );
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    final repos=ref.watch(repoProvider);
-    final auth=ref.watch(authProvider);
-    authStatus=auth.authStatus;
+    final repos = ref.watch(repoProvider);
+    final auth = ref.watch(authProvider);
+    authStatus = auth.authStatus;
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Repo List"),
+      appBar: PreferredSize(
+        preferredSize: Size(1.sw, 60.h),
+        child: BgGradientColor(
+          child: AppBar(
+            leading: BackButton(
+              color: Colors.white,
+              style: ButtonStyle(
+                iconSize: WidgetStatePropertyAll(
+                  20.sp,
+                ),
+              ),
+            ),
+            title: SearchBar(
+              autoFocus: false,
+              controller: _searchController,
+              hintText: 'Search your repositories',
+              onChanged: _onSearchChanged,
+              onTapOutside: (_) => FocusScope.of(context).unfocus(),
+              trailing: [
+                _searchController.text.isEmpty
+                    ? const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Icon(Icons.search),
+                      )
+                    : IconButton(
+                        onPressed: () {
+                          _searchController.clear();
+                          setState(() {
+                            _filteredRepos.clear();
+                            _filteredRepos.addAll(repos.repositories);
+                          });
+                        },
+                        icon: const Icon(Icons.clear),
+                      ),
+              ],
+            ),
+            backgroundColor: Colors.transparent,
+          ),
+        ),
       ),
-      body: !(authStatus==AuthStatus.authenticated)?Text("Login with GitHub first"):
-      repos.isLoading
+      body: !(authStatus == AuthStatus.authenticated)
+          ? Text("Login with GitHub first")
+          : repos.isLoading
               ? Center(child: CircularProgressIndicator())
               : repos.hasError
                   ? Center(child: Text('Failed to load repositories'))
                   : ListView.builder(
-                      itemCount: repos.repositories.length,
+                      itemCount: _filteredRepos.length,
                       itemBuilder: (context, index) {
-                        final repo = repos.repositories[index];
+                        final repo = _filteredRepos[index];
                         return Card(
                           elevation: 0,
                           child: ListTile(
@@ -65,18 +125,24 @@ class _RepoListScreenState extends ConsumerState<RepoListScreen> {
                                                   name: repo["name"],
                                                   fullName: repo["full_name"],
                                                   owner: Owner(
-                                                      login: repo["owner"]["login"],
+                                                      login: repo["owner"]
+                                                          ["login"],
                                                       id: repo["owner"]["id"],
-                                                      avatarUrl: repo["owner"]["avatar_url"]),
+                                                      avatarUrl: repo["owner"]
+                                                          ["avatar_url"]),
                                                   private: repo["private"],
-                                                  defaultBranch: repo['default_branch'],
+                                                  defaultBranch:
+                                                      repo['default_branch'],
                                                   permissions: Permissions(
-                                                      admin: repo['permissions']['admin'],
-                                                      push: repo['permissions']['push'],
-                                                      pull: repo['permissions']['pull'])),
+                                                      admin: repo['permissions']
+                                                          ['admin'],
+                                                      push: repo['permissions']
+                                                          ['push'],
+                                                      pull: repo['permissions']
+                                                          ['pull'])),
                                               ops: GitOperations(
                                                   token: repos.token),
-                                          path: "/",
+                                              path: "/",
                                             )));
                               }),
                         );
