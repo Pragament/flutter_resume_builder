9055eaaf6a0788a69f718055769661f2ccba1633
Insaf-Finser <insaffinser@gmail.com>
2025-07-20 23:24:39 +0530

    Auth Gitlab

--- lib/auth_provider.dart
+++ lib/auth_provider.dart
@@ -1,13 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:resume_builder_app/shared_preferences.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:oauth2/oauth2.dart' as oauth2;
-import 'package:url_launcher/url_launcher.dart';
-import 'package:app_links/app_links.dart';
+import 'package:flutter_web_auth_2/flutter_web_auth_2.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'dart:async';
 
 enum AuthStatus {
@@ -72,7 +73,7 @@ class AuthProvider extends ChangeNotifier {
   }
 
   static Future<UserCredential?> signInWithGitLab() async {
-    const String clientId = 'YOUR GITLABID';
+    const String clientId = 'CLIENT ID';
     const String clientSecret = 'SECRET'; // If needed
     const String redirectUri = 'REDIRECT URL IN THE GITLAB';
     const String authorizationEndpoint = 'https://gitlab.com/oauth/authorize';
@@ -80,46 +81,38 @@ class AuthProvider extends ChangeNotifier {
     const List<String> scopes = ['read_user','api'];
 
     try {
-      // Step 1: Start OAuth2 flow
-      final grant = oauth2.AuthorizationCodeGrant(
-        clientId,
-        Uri.parse(authorizationEndpoint),
+      final url =
+        '$authorizationEndpoint?client_id=$clientId&redirect_uri=$redirectUri&response_type=code&scope=${scopes.join(' ')}';
+
+      final result = await FlutterWebAuth2.authenticate(
+        url: url,
+        callbackUrlScheme: 'com.pragament.resumebuilder',
+      );
+
+      final code = Uri.parse(result).queryParameters['code'];
+      if (code == null) throw Exception('No code returned from GitLab');
+
+      final response = await http.post(
         Uri.parse(tokenEndpoint),
-        secret: clientSecret,
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        body: {
+          'client_id': clientId,
+          'client_secret': clientSecret,
+          'code': code,
+          'grant_type': 'authorization_code',
+          'redirect_uri': redirectUri,
+        },
       );
-
-      final authorizationUrl = grant.getAuthorizationUrl(
-        Uri.parse(redirectUri),
-        scopes: scopes,
-      );
-
-      await launchUrl(authorizationUrl, mode: LaunchMode.externalApplication);
-
-      // Listen for the redirect
-      final completer = Completer<oauth2.Client>();
-      late StreamSubscription sub;
-      sub = AppLinks().uriLinkStream.listen((Uri? uri) async {
-        if (uri != null && uri.toString().startsWith(redirectUri)) {
-          try {
-            final client = await grant.handleAuthorizationResponse(uri.queryParameters);
-            completer.complete(client);
-          } catch (e) {
-            completer.completeError(e);
-          }
-          await sub.cancel();
-        }
-      });
-
-      final oauthClient = await completer.future;
-      final gitlabAccessToken = oauthClient.credentials.accessToken;
+      if (response.statusCode != 200) throw Exception('Failed to get access token');
+      final Map<String, dynamic> tokenData = jsonDecode(response.body);
+      final gitlabAccessToken = tokenData['access_token'];
 
       // Step 2: Exchange GitLab token for Firebase custom token
       final HttpsCallable callable = _functions.httpsCallable('createCustomTokenFromGitLab');
-      final result = await callable.call(<String, dynamic>{
+      final resultToken = await callable.call(<String, dynamic>{
         'gitlabAccessToken': gitlabAccessToken,
       });
-
-      final String firebaseCustomToken = result.data['token'];
+      final String firebaseCustomToken = resultToken.data['token'];
 
       // Step 3: Sign in to Firebase with custom token
       final UserCredential userCredential = await _firebaseAuth.signInWithCustomToken(firebaseCustomToken);
@@ -129,7 +122,12 @@ class AuthProvider extends ChangeNotifier {
 
       return userCredential;
     } catch (e) {
-      print('GitLab + Firebase sign-in error: $e');
+      if (e is PlatformException && e.code == 'CANCELED') {
+        // User canceled login, show a message or ignore
+        print('User canceled GitLab login');
+      } else {
+        print('GitLab + Firebase sign-in error: $e');
+      }
       return null;
     }
   }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -5,6 +5,7 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   app_links
   cloud_firestore
+  desktop_webview_window
   file_selector_windows
   firebase_auth
   firebase_core
@@ -12,6 +13,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   printing
   share_plus
   url_launcher_windows
+  window_to_front
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
